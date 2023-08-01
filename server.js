4ac7751adf0b9193a6cbff45c0e863dbb7e4c18b
AsumVictor <iamasum369@gmail.com>
2023-08-01 10:24:12 +0000

    Added cloudinary

--- server.js
+++ server.js
@@ -27,3 +27,9 @@ process.on("onhandleRejection", (err) => {
     process.exit(1);
   });
 });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
